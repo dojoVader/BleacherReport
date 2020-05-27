9439a806aaa264b4d4e01b1e268efe73b20cc104
okeowo.aderemi <okeowoaderemi@gmail.com>
2020-05-27 12:44:07 +0100

    Structure has been created for the application in TS

--- bleacher-report/src/App.tsx
+++ bleacher-report/src/App.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+export default class App extends React.Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    render() {
+        return (
+            <div className="main-app col-md-12 col-lg-12">
+                Piano app goes here...ok
+            </div>
+        );
+    }
 }
-
-export default App;
---
+++ bleacher-report/src/component/Piano/Paino.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Piano extends React.Component {
+    render(){
+        return (
+            <div>
+                Paino apps shows here...op
+            </div>
+        );
+    }
+}
